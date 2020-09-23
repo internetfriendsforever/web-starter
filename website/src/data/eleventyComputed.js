b71e8aa5c097703e74c1403ed355c937b5fbbcb3
Daniel Mahal <danielmahal@gmail.com>
2020-09-23 19:34:43 +0200

    Add localized template paths with mappings

--- website/src/data/eleventyComputed.js
+++ website/src/data/eleventyComputed.js
@@ -1,28 +1,66 @@
-const { getTranslation } = require('../utils/i18n')
+const locales = require('./locales.json')
+
+const langs = locales.map(locale => locale.lang)
+
+function getTranslation (object, lang) {
+  return object[lang] || object[langs.find(lang => lang in object)]
+}
 
 module.exports = {
-  locale: {
-    lang: data => data.page.filePathStem.split('/').filter(Boolean)[0],
+  locale: data => {
+    // Find current lang based on template file path (e.g. /en/about)
+    const lang = data.page.filePathStem.split('/').filter(Boolean)[0]
 
-    strings: data => {
-      return Object.keys(data.strings).reduce((result, key) => {
-        result[key] = getTranslation(data.strings[key], data.locale.lang)
-        return result
-      }, {})
-    },
+    // Translate strings
+    const strings = Object.keys(data.strings).reduce((result, key) => {
+      result[key] = getTranslation(data.strings[key], lang)
+      return result
+    }, {})
 
-    articles: data => {
-      return data.articles.map(article => ({
-        ...article,
-        introduction: getTranslation(article.introduction, data.locale.lang)
-      }))
-    },
+    // Translate articles
+    const articles = data.articles.map(article => ({
+      ...article,
+      introduction: getTranslation(article.introduction, lang)
+    }))
 
-    locales: data => {
-      return data.locales.map(locale => ({
+    // Find path mappings to other locales
+    // Check if locale is the current one (for highlighting)
+    const locales = data.locales.map(locale => {
+      const mappings = {}
+
+      data.navigation.forEach(item => {
+        mappings[getTranslation(item.path, lang)] = item.path
+      })
+
+      const localePath = data.page.url.replace(new RegExp('^/' + lang), '')
+      const mapping = mappings[localePath]
+
+      return {
         ...locale,
-        current: locale.lang === data.locale.lang
-      }))
+        path: mapping ? getTranslation(mapping, locale.lang) : '',
+        current: locale.lang === lang
+      }
+    })
+
+    // Translate navigation labels
+    // Check if navigation page is the current one (for highlighting)
+    const navigation = data.navigation.map(item => {
+      const path = `/${lang}${getTranslation(item.path, lang)}`
+
+      return {
+        ...item,
+        path,
+        label: getTranslation(item.label, lang),
+        current: data.page.url === path
+      }
+    })
+
+    return {
+      lang,
+      strings,
+      articles,
+      locales,
+      navigation
     }
   }
 }
--- website/src/utils/i18n.js
+++
@@ -1,9 +0,0 @@
-const fallbacks = ['en', 'de']
-
-function getTranslation (object, lang) {
-  return object[lang] || object[fallbacks.find(lang => lang in object)]
-}
-
-module.exports = {
-  getTranslation
-}
