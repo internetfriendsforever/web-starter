c4185fb3bb264a0a2d0c0c5940ccd779809a3b9c
Daniel Mahal <danielmahal@gmail.com>
2020-09-25 11:27:15 +0200

    Don't use localize for mapping paths!

--- website/src/views/about.data.js
+++ website/src/views/about.data.js
@@ -1,13 +1,12 @@
-const localize = require('../utils/localize')
-
 module.exports = {
   eleventyComputed: {
     navigation: data => ({
       ...data.navigation,
 
-      // Overwrite locale navigation with paths to page in other locales
       locales: data.navigation.locales.map(item => {
-        const path = item.path + localize(data.navigation.pages.about.path, item.locale)
+        const lang = item.locale.lang
+        const pagePath = data.navigation.pages.about.path[lang]
+        const path = pagePath ? `/${item.locale.lang}/${pagePath}` : item.path
         return { ...item, path }
       })
     })
--- website/src/views/article.data.js
+++ website/src/views/article.data.js
@@ -1,19 +1,18 @@
 module.exports = {
   eleventyComputed: {
     navigation: data => {
-      // Find the article which is currently being rendered
       const slug = data.page.url.split('/').filter(Boolean)[0]
       const article = data.articles.find(article => article.slug.local === slug)
 
       return {
         ...data.navigation,
 
-        // Overwrite locale navigation with paths to page in other locales
         locales: data.navigation.locales.map(item => {
           const lang = item.locale.lang
-          const path = `/${lang}/${article.slug[lang]}/`
+          const slug = article.slug[lang]
+          const path = slug ? `/${lang}/${slug}/` : item.path
           return { ...item, path }
-        })
+        }).filter(Boolean)
       }
     }
   }
