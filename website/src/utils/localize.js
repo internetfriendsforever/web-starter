0ad7d517d62aac63a411caf68a96303fda81736f
Daniel Mahal <danielmahal@gmail.com>
2020-09-25 09:18:14 +0200

    Fallback to any other language prioritized by locales.json

--- website/src/utils/localize.js
+++ website/src/utils/localize.js
@@ -1,3 +1,6 @@
+const locales = require('../data/locales.json')
+
 module.exports = function (object, locale) {
-  return object[process.env.lang] || object.en
+  return object[process.env.lang] ||
+    object[locales.find(locale => locale.lang in object).lang]
 }
