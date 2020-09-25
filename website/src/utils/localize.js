a87af4ae5e612463f2793f581cb8c670c447f618
Daniel Mahal <danielmahal@gmail.com>
2020-09-25 10:27:39 +0200

    Clean up and comment page localization mapping

--- website/src/utils/localize.js
+++ website/src/utils/localize.js
@@ -1,9 +1,13 @@
 const lang = require('../data/lang')
 const locales = require('../data/locales.json')
 
-module.exports = function (object) {
-  const local = object[lang] ||
-      object[locales.find(locale => locale.lang in object).lang]
+module.exports = function (object, locale) {
+  let local = object[locale ? locale.lang : lang]
+
+  // Fallback to first translation prioritized by locales.json
+  if (!local) {
+    local = object[locales.find(locale => locale.lang in object).lang]
+  }
 
   return {
     ...object,
--- website/src/views/about.data.js
+++ website/src/views/about.data.js
@@ -1,10 +1,11 @@
+const localize = require('../utils/localize')
+
 module.exports = {
   eleventyComputed: {
     localeNavigation: data => {
       // Overwrite localeNavigation with paths to about page in other locales
       return data.localeNavigation.map(item => {
-        const lang = item.locale.lang
-        const path = `/${lang}${data.pageNavigation.about.path[lang]}`
+        const path = item.path + localize(data.pageNavigation.about.path, item.locale)
         return { ...item, path }
       })
     }
