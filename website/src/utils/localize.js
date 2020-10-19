f329ce5d2677f608b735281ad24c4cdc107bc726
Daniel Mahal <danielmahal@gmail.com>
2020-10-19 14:36:34 +0200

    Return null instead of failing when no fallback is present in the object

--- website/src/utils/localize.js
+++ website/src/utils/localize.js
@@ -3,13 +3,13 @@ const lang = require('../data/lang')
 const locales = require('../data/locales')
 
 // Localize object
-module.exports.object = (object, locale) => ({
-  ...object,
-  local: (
-    object[locale ? locale.lang : lang] ||
-    object[locales.find(locale => locale.lang in object).lang]
-  )
-})
+module.exports.object = (object, locale) => {
+  const direct = object[locale ? locale.lang : lang]
+  const fallbackLocale = locales.find(locale => locale.lang in object)
+  const fallback = fallbackLocale ? object[fallbackLocale.lang] : null
+  const local = direct || fallback || null
+  return { ...object, local }
+}
 
 // Localize Date object
 module.exports.date = function (date, format, locale) {
