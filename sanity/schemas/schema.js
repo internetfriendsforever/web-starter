793952c515c24197f4dc3ada782bab441ff76c9e
s-g-k <seb@internetfriendsforever.com>
2021-04-22 10:03:09 +0200

    Separate article out to its own file

--- sanity/schemas/schema.js
+++ sanity/schemas/schema.js
@@ -1,29 +1,10 @@
 import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
+import article from './documents/article'
 
 export default createSchema({
   name: 'default',
   types: schemaTypes.concat([
-    {
-      name: 'article',
-      type: 'document',
-      fields: [
-        {
-          name: 'title',
-          type: 'string'
-        },
-        {
-          name: 'image',
-          type: 'image'
-        },
-        {
-          name: 'body',
-          type: 'array',
-          of: [
-            { type: 'block' }
-          ]
-        }
-      ]
-    }
+    article
   ])
 })
