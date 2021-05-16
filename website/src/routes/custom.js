7b513c1044b2f2ec4059ed849b868bec684f3bc1
s-g-k <seb@internetfriendsforever.com>
2021-05-17 01:43:38 +0200

    Add layout to custom page

--- website/src/routes/custom.js
+++ website/src/routes/custom.js
@@ -6,36 +6,39 @@ const counter = require('../partials/counter')
 module.exports = () => layout({
   title: 'Custom',
   content: html`
-    <a href="/">Back to home</a>
+    <div class="stack">
+      <nav class="nav-breadcrumbs">
+        <a href="/">Back to home</a>
+      </nav>
 
-    <h1>Custom route</h1>
+      <h1>Custom route</h1>
+      <h2>Async partial</h2>
 
-    <h2>Async partial</h2>
+      ${number()}
 
-    ${number()}
+      <h2>Array of async partials</h2>
 
-    <h2>Array of async partials</h2>
+      ${[
+        number(),
+        number(),
+        number()
+      ]}
 
-    ${[
-      number(),
-      number(),
-      number()
-    ]}
+      <h2>Client script</h2>
 
-    <h2>Client script</h2>
+      <h3>Counter 1</h3>
 
-    <h3>Counter 1</h3>
+      ${counter({
+        id: 'counter-1',
+        value: 10
+      })}
 
-    ${counter({
-      id: 'counter-1',
-      value: 10
-    })}
+      <h3>Counter 2</h3>
 
-    <h3>Counter 2</h3>
-
-    ${counter({
-      id: 'counter-2',
-      value: 20
-    })}
+      ${counter({
+        id: 'counter-2',
+        value: 20
+      })}
+    </div>
   `
 })
